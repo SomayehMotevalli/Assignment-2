ac120ed5d9c407cdb13fc499dbca88d8f48a4252
SomayehMotevalli <somayeh.motevalli@gmail.com>
2024-11-06 17:36:58 -0800

    test1

--- test/test_logreg.py
+++ test/test_logreg.py
@@ -5,10 +5,50 @@ Write your logreg unit tests here. Some examples of tests we will be looking for
 
 More details on potential tests below, these are not exhaustive
 """
-import pytest
+
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 from regression import (logreg, utils)
-print(logreg.__file__)
+from sklearn.preprocessing import StandardScaler
+
+def test_updates():
+
+    # load data with default settings
+    X_train, X_val, y_train, y_val = utils.loadDataset(features=['Penicillin V Potassium 500 MG', 'Computed tomography of chest and abdomen', 
+                                                                  'Plain chest X-ray (procedure)',  'Low Density Lipoprotein Cholesterol',
+                                                                  'Creatinine', 'AGE_DIAGNOSIS'], split_percent=0.8, split_state=42)
+
+    # scale data since values vary across features
+    sc = StandardScaler()
+    X_train = sc.fit_transform(X_train)
+    X_val = sc.transform (X_val)
+    
+    print(X_train.shape, X_val.shape, y_val.shape, y_train.shape)
+
+
+    """
+    # for testing purposes once you've added your code
+    # CAUTION & HINT: hyperparameters have not been optimized
+
+    log_model = logreg.LogisticRegression(num_feats=2, max_iter=10, tol=0.01, learning_rate=0.00001, batch_size=12)
+    log_model.train_model(X_train, y_train, X_val, y_val)
+    log_model.plot_loss_history()
+            
+    """
+    log_model = logreg.LogisticRegression(num_feats=6, max_iter=10, tol=0.01, learning_rate=0.01, batch_size=12)
+    log_model.train_model(X_train, y_train, X_val, y_val)
+	
+
+    
+	
+
+
+
+# import pytest
+# import numpy as np
+# from regression import (logreg, utils)
+# print(logreg.__file__)
 
 def test_updates():
 	# Check that your gradient is being calculated correctly
